3c67635445530ad3ef94536d74b87236b18d2818
sakthiarajapandian <sakthiapandian27@gmail.com>
2024-09-24 15:50:41 +1000

    push latest

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -1,2 +1,4 @@
-#!/bin/bash
-docker-compose push
+echo $REGISTRY_PW | docker login $CONTAINER_REGISTRY --username $REGISTRY_UN --password-stdin
+docker push book-catalog
+docker push inventory-management
+ 
