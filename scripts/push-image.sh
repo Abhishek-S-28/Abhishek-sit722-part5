b7fd6a85703941df95b600818c33d927ac98b069
sakthiarajapandian <sakthiapandian27@gmail.com>
2024-10-02 20:39:07 +1000

    latest

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -1,3 +1,3 @@
 echo $REGISTRY_PW | docker login $CONTAINER_REGISTRY --username $REGISTRY_UN --password-stdin
-docker push book-catalog
-docker push inventory-management
+docker push bookinventory.azurecr.io/book-catalog
+docker push bookinventory.azurecr.io/inventory-management
