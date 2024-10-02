765b6408d22b90f6fd7bc2d0f39f64c6392c7be7
sakthiarajapandian <sakthiapandian27@gmail.com>
2024-10-02 20:16:21 +1000

    change

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker-compose build
+docker build -t book-catalog --file ./book_catalog/Dockerfile ./book_catalog
+docker build -t inventory-management --file ./inventory_management/Dockerfile ./inventory_management
