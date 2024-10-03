86e057d56b7c1a6b0fa0054c4061774fd3eb05a1
Abhishek Srinivasan <abhisheksrini2001@gmail.com>
2024-10-03 16:38:43 +1000

    push

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker build -t book-catalog --file ./book_catalog/Dockerfile ./book_catalog
-docker build -t inventory-management --file ./inventory_management/Dockerfile ./inventory_management
-docker tag book-catalog bookinventory.azurecr.io/book-catalog
-docker tag inventory-management bookinventory.azurecr.io/inventory-management
+# docker build -t book-catalog --file ./book_catalog/Dockerfile ./book_catalog
+# docker build -t inventory-management --file ./inventory_management/Dockerfile ./inventory_management
+# docker tag book-catalog bookinventory.azurecr.io/book-catalog
+# docker tag inventory-management bookinventory.azurecr.io/inventory-management
--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -1,3 +1,2 @@
 echo $REGISTRY_PW | docker login $CONTAINER_REGISTRY --username $REGISTRY_UN --password-stdin
-docker push bookinventory.azurecr.io/book-catalog
-docker push bookinventory.azurecr.io/inventory-management
+docker push 
